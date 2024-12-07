4ce7171699016c8d84452efe7b86afc075d2f06c
Antony0101 <vimalt24@gmail.com>
2024-12-07 22:50:38 +0530

    added resume to intro

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,6 +2,7 @@ export const LINKS = {
   github: 'https://github.com/Antony0101',
   linkedin: 'www.linkedin.com/in/vimal-tony-babu-6835a91a9',
   boot_dot_dev: 'https://www.boot.dev/u/antony0101',
+  resume: 'https://drive.google.com/file/d/10DMk3H_8vlRlWOYd27K-ouK8uyOTdANy/view?usp=sharing',
 };
 
 export const loaderAnimation = ['.loader', { opacity: [1, 0], pointerEvents: 'none' }, { easing: 'ease-out' }];
