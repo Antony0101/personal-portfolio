9bd33924e85c1753a5be690f72b5c7a03fe0fcd6
Antony0101 <vimalt24@gmail.com>
2024-12-07 11:35:55 +0530

    updated data

---
+++ src/lib/constants.ts
@@ -0,0 +1,7 @@
+export const LINKS = {
+  github: 'https://github.com/Antony0101',
+  linkedin: 'www.linkedin.com/in/vimal-tony-babu-6835a91a9',
+  boot_dot_dev: 'https://www.boot.dev/u/antony0101',
+};
+
+export const loaderAnimation = ['.loader', { opacity: [1, 0], pointerEvents: 'none' }, { easing: 'ease-out' }];
