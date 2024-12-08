18665a1b5eab03feded2357a846f44d90736b8c1
Antony0101 <vimalt24@gmail.com>
2024-12-08 11:07:59 +0530

    fixed linkedin link issue

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: 'https://github.com/Antony0101',
-  linkedin: 'www.linkedin.com/in/vimal-tony-babu-6835a91a9',
+  linkedin: 'https://www.linkedin.com/in/vimal-tony-babu-6835a91a9',
   boot_dot_dev: 'https://www.boot.dev/u/antony0101',
   resume: 'https://drive.google.com/file/d/10DMk3H_8vlRlWOYd27K-ouK8uyOTdANy/view?usp=sharing',
 };
